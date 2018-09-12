fbf180b3a9856033da068ba3efde33b3c3386020
gusheng <2234839456@qq.com>
2018-09-12 12:33:23 +0800

    修改了触发方式，增加了删除元素的方法

--- 元素可编辑 使网页成为wiki.js
+++ 元素可编辑 使网页成为wiki.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         网页文本编辑,做笔记的好选择
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
+// @version      0.13
+// @description  所见即所得！
 // @author       You
 // @match        *
 // @include        *
@@ -11,29 +11,44 @@
 
 (function () {
     'use strict';
-    //禁用页面后退
-    window.addEventListener('popstate', function () {
-        history.pushState(null, null, document.URL);
+    //获取鼠标位置
+    var path;
+    document.addEventListener('mousemove', (event) => {
+        path =event.path;
     });
 
+    //监测 alt+q 事件
+    document.addEventListener('keydown', (event) => {
+        if (!event.altKey)
+            return;
+        switch (event.key) {
+            case "q":editSelect()
+                break;
+            case "Backspace":deleteSelect()
+                break;
+        }
+    });
+    //设置一个影藏的文本框用来复制文本
     const input = document.createElement('input');
     input.setAttribute('type', 'hidden');
     input.setAttribute('readonly', 'readonly');
     document.body.appendChild(input);
-    document.addEventListener('mousedown', (event) => {
-        if (event.button != 3)//鼠标侧后键  这样比较不容易触发其他事件,他本身的功能是前进
-            return
-        event.path[0].setAttribute("contenteditable", "true");
-        //获取元素的描述并将他们添加到剪贴板  目前仅支持mdn
+    function editSelect() {
+        var selectElem = path[0]
+        selectElem.setAttribute("contenteditable", "true");
+        //获取元素的描述并将他们添加到剪贴板  目前支持mdn 其它的可能支持
         var title
-        for (let index = 0; index < event.path.length; index++) {
-            title = event.path[index].getAttribute("title");
+        //这里抛弃后两个元素是因为他们不是一般的elem元素了
+        for (let index = 0; index < path.length - 2; index++) {
+            title = path[index].getAttribute("title");
             if (title)
                 break
         }
-        console.log(title)
         input.setAttribute('value', title);
         input.select();
         document.execCommand('copy');//复制
-    });
+    }
+    function deleteSelect(){
+        path[0].parentNode.removeChild(path[0]);  
+    }
 })();
