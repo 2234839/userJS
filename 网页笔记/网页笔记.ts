24ceb0db0fbb89f239b53427fd816112571c457a
崮生 <admin@shenzilong.cn>
2019-12-04 14:15:32 +0800

    🐛 无法使用

--- 网页笔记/网页笔记.ts
+++ 网页笔记/网页笔记.ts
@@ -8,7 +8,7 @@ import { nodePath } from "./util";
 // ==UserScript==
 // @name         网页文本编辑,做笔记的好选择
 // @namespace    http://tampermonkey.net/
-// @version      1.36
+// @version      1.37
 // @description  所见即所得！
 // @author       崮生 2234839456@qq.com
 // @match        *
