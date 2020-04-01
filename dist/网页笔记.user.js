a283cb3f9b70a84259af55ed56deb5a11a78d8ea
崮生 <admin@shenzilong.cn>
2020-04-01 10:44:57 +0800

    :white_check_mark: 重构代码，为下一步更新做准备

--- dist/网页笔记.user.js
+++ dist/网页笔记.user.js
@@ -4200,7 +4200,7 @@ Object.defineProperty(exports, "__esModule", {
 const app_svelte_1 = __importDefault(require("./app.svelte")); // ==UserScript==
 // @name         网页文本编辑,做笔记的好选择
 // @namespace    http://tampermonkey.net/
-// @version      1.37
+// @version      1.38
 // @description  所见即所得！
 // @author       崮生 2234839456@qq.com
 // @match        *
--- 网页笔记/网页笔记.user.ts
+++ 网页笔记/网页笔记.user.ts
@@ -1,11 +1,9 @@
 import App from "./app.svelte";
-import { CommandControl } from "./function/command";
-import { setLocalItem } from "./lib/store";
 
 // ==UserScript==
 // @name         网页文本编辑,做笔记的好选择
 // @namespace    http://tampermonkey.net/
-// @version      1.37
+// @version      1.38
 // @description  所见即所得！
 // @author       崮生 2234839456@qq.com
 // @match        *
