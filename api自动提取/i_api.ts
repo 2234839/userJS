06c4949fc69ea4d967a57841f429ac58c37c29c0
崮生 <admin@shenzilong.cn>
2020-04-29 14:33:54 +0800

    :bug: 对数组类型的处理不正确

--- api自动提取/i_api.ts
+++ api自动提取/i_api.ts
@@ -1,22 +1,22 @@
 /** api的接口 */
 export interface api {
-    url: string
-    name: string
-    describe: string
-    method: string
-    parList:par[],
-    resList: par[],
+  url: string;
+  name: string;
+  describe: string;
+  method: string;
+  parList: par[];
+  resList: par[];
 }
 
-export interface par{
-    name: string
-    /** 是否必需 */
-    must: boolean
-    type: any
-    /** 描述 */
-    describe: string,
-    /** 参数所在的位置 */
-    place?:"query" | "body"
-    /** 可能存在子级的参数 */
-    children?:par[]
+export interface par {
+  name: string;
+  /** 是否必需 */
+  must: boolean;
+  type: string;
+  /** 描述 */
+  describe: string;
+  /** 参数所在的位置 */
+  place?: "query" | "body";
+  /** 可能存在子级的参数 */
+  children?: par[];
 }
--- dist/api自动提取.user.js
+++ dist/api自动提取.user.js
@@ -435,20 +435,22 @@ exports.apiToTypeScriptCode = apiToTypeScriptCode;
 /** 解析api的par为字符串 */
 
 function parse_par_item(par, level = 0) {
-  return `${util_1.copyStr('\t', level)}/** ${par.type} ${par.describe} */${par.name}${par.must ? '' : '?'}: ${(() => {
-    if (par.children === undefined) return par.type.replace("string(", '_string(')
+  return `${util_1.copyStr("\t", level)}/** ${par.type} ${par.describe} */${par.name}${par.must ? "" : "?"}: ${(() => {
+    if (par.children === undefined) return par.type.replace("string(", "_string(")
     /** string 这种基本类型不能够使用引用的方式解决，所以加上一个_来区分 */
-    .replace("number(", '_number(').replace("String", 'string')
+    .replace("number(", "_number(").replace("String", "string")
     /** 基元类型不要用 */
-    .replace("Number", 'number').replace("Boolean", 'boolean').replace('(', '<').replace(')', '>').replace('-', '_');
-    return `${parse_par_List(par.children, level + 1)}`;
+    .replace("Number", "number").replace("Boolean", "boolean").replace("(", "<").replace(")", ">").replace("-", "_");
+    return `${parse_par_List(par.children, level + 1)}${
+    /** 处理数组类型 */
+    par.type.includes("[]") ? "[]" : ""}`;
   })()}`;
 }
 /** 解析api的par数组为字符串 */
 
 
 function parse_par_List(par, level = 1) {
-  return `{\r${par.map(el => parse_par_item(el, level)).join(',\n')}}`;
+  return `{\r${par.map(el => parse_par_item(el, level)).join(",\n")}}`;
 }
 },{"../util":"util.ts"}],"parse/showDocApi.ts":[function(require,module,exports) {
 "use strict";
