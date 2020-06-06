2219b717612e3f8a3906a0ae93b1e4672a456ace
崮生 <admin@shenzilong.cn>
2020-06-06 09:24:58 +0800

    ✨feat(ansi_to_html): 新增这个脚本，可以美化ansi

---
+++ ansi_to_html/ansi_to_html.user.ts
@@ -0,0 +1,28 @@
+import { default as AnsiUp } from "ansi_up";
+
+const ansi_up = new AnsiUp();
+// ==UserScript==
+// @name         ansi-to-html
+// @namespace    http://tampermonkey.net/
+// @version      1.1.2
+// @description
+// @author       崮生 2234839456@qq.com
+// @include      *://www.showdoc.cc/*
+// @grant        unsafeWindow
+// @connect      shenzilong.cn
+// ==/UserScript==
+import "./ansi_to_html.css";
+(async function () {
+  const btn = document.createElement("button");
+  btn.textContent="美化输出"
+  btn.classList.add("llej_userjs_ansi-btn");
+  document.body.appendChild(btn);
+  btn.addEventListener("click", function () {
+    Array.from(document.querySelectorAll(".console-output")).forEach((el) => {
+      const html = ansi_up.ansi_to_html(el.innerHTML);
+      console.log(el, html);
+      el.classList.add("llej_userjs_ansi-pre");
+      el.innerHTML = html;
+    });
+  });
+})();
