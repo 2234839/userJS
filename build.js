4daa8e8cc9561b7f1f0f4c0793befec49a5bfd71
崮生 <admin@shenzilong.cn>
2020-07-21 10:22:27 +0800

    🐎ci(build): 使用node执行命令

---
+++ build.js
@@ -0,0 +1,19 @@
+var exec = require("child_process").execSync;
+function run(str) {
+  process.stdout.write(exec(str));
+}
+
+const project = [
+  "./自动同步文章/自动同步文章.ts",
+  "./网页笔记/网页笔记.user.ts",
+  "./祭奠百度/祭奠百度.user.ts",
+  "./请求代理/请求代理.user.ts",
+  "./api自动提取/api自动提取.user.ts",
+  "./文本分类/文本分类.user.ts",
+  "./去除顶部遮挡/去除顶部遮挡.user.ts",
+  "./请求代理/请求代理.user.ts",
+  "./高级复制/高级复制.user.ts",
+];
+project.forEach((el) => {
+  run(`npx parcel build --no-minify --no-source-maps  ${el}`);
+});
