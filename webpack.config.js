97e787bbd5c5edfe2b4b6506d2b39b8dbba6c024
崮生 <admin@shenzilong.cn>
2020-09-14 18:15:40 +0800

    🔧build(build): 网页笔记改为使用webpack

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const webpack = require("webpack");
+const fs = require("fs");
+const entry = path.resolve(__dirname, "./网页笔记/网页笔记.user.ts");
+const text = fs.readFileSync(entry, { encoding: "utf8" });
+
+const meta = text.match(/\/\/ ==UserScript==[\s\S]+\/\/ ==\/UserScript==/g)[0];
+module.exports = {
+  mode: "development",
+  entry: entry,
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  resolve: {
+    alias: {
+      svelte: path.resolve("node_modules", "svelte"),
+    },
+    extensions: [".ts", ".tsx", ".mjs", ".js", ".svelte"],
+    mainFields: ["svelte", "browser", "module", "main"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto",
+      },
+      {
+        test: /\.(html|svelte)$/,
+        exclude: /node_modules/,
+        use: "svelte-loader",
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `${meta}\n// 以下代码是打包后的代码，可以去 https://github.com/2234839/userJS 查看正常代码`,
+      raw: true,
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    disableHostCheck: true,
+    host: "0.0.0.0",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+    },
+  },
+  devtool: "inline-source-map",
+};
