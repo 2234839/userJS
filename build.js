2190b8c83df03b1250ef79a28bc69ea86e069b72
崮生 <admin@shenzilong.cn>
2020-10-30 11:00:25 +0800

    📃docs(请求代理): 修改文档

--- build.js
+++ build.js
@@ -6,12 +6,10 @@ function run(str) {
 
 const project = [
   "./祭奠百度/祭奠百度.user.ts",
-  "./请求代理/请求代理.user.ts",
   "./自动同步文章/自动同步文章.ts",
   "./api自动提取/api自动提取.user.ts",
   "./文本分类/文本分类.user.ts",
   "./去除顶部遮挡/去除顶部遮挡.user.ts",
-  "./请求代理/请求代理.user.ts",
   "./高级复制/高级复制.user.ts",
 ];
 project.forEach((el) => {
