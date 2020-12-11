1a41380c7d0159721d11d526b0a47d7b9573e0b9
崮生 <admin@shenzilong.cn>
2020-12-11 15:58:15 +0800

    🐞fix(网页笔记):
    调试函数挂载错误

--- 网页笔记/网页笔记.user.ts
+++ 网页笔记/网页笔记.user.ts
@@ -8,7 +8,7 @@ import { editElement } from "./state/index";
 // ==UserScript==
 // @name         网页文本编辑,做笔记的好选择
 // @namespace    http://tampermonkey.net/
-// @version      1.43
+// @version      1.44
 // @description  所见即所得！
 // @author       崮生 2234839456@qq.com
 // @match        *
@@ -20,7 +20,7 @@ import { editElement } from "./state/index";
 // @grant        GM.xmlHttpRequest
 // ==/UserScript==
 (async function () {
-  let global = (typeof unsafeWindow !== "undefined" ? window : unsafeWindow) as typeof window & {
+  let global = (typeof unsafeWindow === "undefined" ? window : unsafeWindow) as typeof window & {
     CommandControl: typeof CommandControl;
     /** 清除当前页面的缓存 */
     llej_pageNotes_clearCurrentStore: () => void;
